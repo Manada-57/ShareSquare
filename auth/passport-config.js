be4b2337cf2a23a83ccb6192a9d6b2d4ed2b9dd3
Manada <jseetharaman07@gmail.com>
2025-09-11 08:38:03 +0530

    Deployed update project

--- auth/passport-config.js
+++ auth/passport-config.js
@@ -19,8 +19,6 @@ passport.deserializeUser(async (id, done) => {
     done(err);
   }
 });
-
-// GOOGLE STRATEGY
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
@@ -47,8 +45,6 @@ passport.use(new GoogleStrategy({
     done(err);
   }
 }));
-
-// GITHUB STRATEGY
 passport.use(new GitHubStrategy({
   clientID: process.env.GITHUB_CLIENT_ID,
   clientSecret: process.env.GITHUB_CLIENT_SECRET,
