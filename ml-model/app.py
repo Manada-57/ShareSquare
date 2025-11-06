40d4c7d845dc54782323e7b132f03bae2902b75b
Manada <jseetharaman07@gmail.com>
2025-11-06 12:08:13 +0530

    Deploy6

--- ml-model/app.py
+++ ml-model/app.py
@@ -66,4 +66,6 @@ def search():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+  import os
+  port = int(os.environ.get("PORT", 5000))  # Render gives this env var dynamically
+  app.run(host="0.0.0.0", port=port, debug=True)
